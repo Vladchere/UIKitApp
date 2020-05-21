c1767dab5422ebdc25a420028681e49cd24cdea3
Vladchere <vladchere.ok@mail.ru>
2020-05-22 00:54:10 +0700

    Initial Commit

---
+++ My UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My UIKitApp
+//
+//  Created by Vladislav on 22.05.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
